faeea9f7169582ba1c6c9bf2e534a59678ff9ff7
Apechai <matthew.fong@gmail.com>
2012-07-11 14:44:55 +1000

    adding Xcode project files

---
+++ Jul12/Jul12AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jul12AppDelegate.h
+//  Jul12
+//
+//  Created by Matthew Fong on 7/11/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jul12AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
